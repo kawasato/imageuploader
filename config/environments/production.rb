1862f82e6a139094ef720e7eec0a096064c1b927
satomi <satomi.kawasaki0607@gmail.com>
2018-12-23 23:45:58 +0900

    production.rbの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'protected-brushlands-48518'}
+  config.action_mailer.default_url_options = { host: 'protected-brushlands-48518.herokuapp.com'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
